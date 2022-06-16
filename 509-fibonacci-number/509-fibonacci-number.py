5bed2345191e24a80c054fe28e092daae9dd1a12
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-06-16 18:55:48 +0300

    Time: 35 ms (82.66%), Space: 13.9 MB (9.01%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,11 @@
+from functools import cache
 class Solution:
     def fib(self, n: int) -> int:
-        dic={0:0,1:1}
-        if n not in dic:
-            dic[n]=self.fib(n-1) + self.fib(n-2)
-        return dic[n]
+        @cache
+        def recurse(n):
+            if n<2:
+                return n
+            else:
+                return recurse(n-1) + recurse(n-2)
+        return recurse(n)
         
