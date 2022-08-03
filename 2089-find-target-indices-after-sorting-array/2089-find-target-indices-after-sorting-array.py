9ff4c1fd5b429297748fa34f20f639b90bf3eea2
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-08-03 21:44:44 +0300

    Time: 106 ms (8.63%), Space: 14.1 MB (17.89%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,13 +1,32 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        final=[]
-        for i in range (len(nums)-1):
-            m_val=min(nums[i:])
-            m_in=nums.index(m_val,i)
-            if nums[i]!=nums[m_in]:
-                nums[i],nums[m_in]=nums[m_in],nums[i]
-        for i in range(len(nums)):
-            if nums[i]==target:
-                final.append(i)
-        return final        
-         
+        list1=[]
+        def merge(lft,rgt):
+            new_list=[]
+            i,j=0,0
+            while len(new_list)<(len(lft)+len(rgt)):
+                if lft[i]<=rgt[j]:
+                    new_list.append(lft[i])
+                    i+=1
+                else:
+                    new_list.append(rgt[j])
+                    j+=1
+                if j>=len(rgt):
+                    new_list+=lft[i:]
+                elif i>=len(lft):
+                    new_list+=rgt[j:]          
+            return new_list 
+        def mergesort(list_):
+            if len(list_)==1:
+                return list_
+            half=len(list_)//2
+            lft=list_[:half]
+            rgt=list_[half:]  
+            lft=mergesort(lft)
+            rgt=mergesort(rgt)
+            return merge(lft,rgt)
+        num=mergesort(nums)
+        for i in range(len(num)):
+            if num[i]==target:
+                list1.append(i)
+        return list1       
