130c7a5c91939175c02930b937c255a42838121f
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-08-04 12:31:46 +0300

    Time: 58 ms (37.27%), Space: 13.8 MB (63.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,9 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)-1):
-            for j in range(i,len(nums)):
-                if nums[j]<nums[i]:
-                    nums[j],nums[i]=nums[i],nums[j]
-        return nums            
+        lft,dummy,rgt=0,0,len(nums)-1
+        while dummy<=rgt:
+            if nums[dummy]==0:
+                nums[lft],nums[dummy]=nums[dummy],nums[lft]
+                lft+=1
+                dummy+=1
+            elif nums[dummy]==1:
+                dummy+=1
+            else:
+                nums[rgt],nums[dummy]=nums[dummy],nums[rgt]
+                rgt-=1
+        return nums       
         
