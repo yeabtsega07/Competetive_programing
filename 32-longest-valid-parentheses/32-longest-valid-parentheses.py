86dcd530ed7d4a1caf43ace8c60cbf4a2fd81f8c
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-08-19 13:31:57 +0300

    Time: 66 ms (59.33%), Space: 14.6 MB (61.86%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,26 +1,16 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         leng=0
-        lft,rgt=0,0
-        for i in range(len(s)-1):
-            if s[i]=="(" :
-                lft+=1
-            elif s[i]==")":
-                rgt+=1
-            if lft==rgt:
-                leng=max(leng,(lft+rgt))
-            elif rgt>=lft:
-                lft,rgt=0,0    
-        lft,rgt=0,0 
-        for i in range(len(s)-1,-1,-1):
-            if s[i]=="(":
-                lft+=1
-            elif s[i]==")":
-                rgt+=1 
-            if lft==rgt:
-                leng=max(leng,(lft+rgt))
-            elif lft>=rgt:
-                lft,rgt=0,0    
+        stack=[-1]
+        for i ,char in enumerate(s):
+            if char=="(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if stack:
+                    leng=max(leng,i-stack[-1])
+                else:
+                    stack.append(i)
         return leng         
             
                 
