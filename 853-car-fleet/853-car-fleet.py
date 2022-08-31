d9bd895b40f5b4ed6c4892ab9ec47edc05c7f0c6
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-08-31 16:58:58 +0300

    Time: 1514 ms (49.92%), Space: 33.7 MB (74.63%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -2,11 +2,11 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pos=[[position[i],speed[i]] for i in range(len(speed))]
         mon_stack=[]
-        pos.sort(key=lambda x:x[0])
+        pos.sort(key=lambda x:x[0], reverse=True)
         for ps in pos:
             time=(target-ps[0])/ps[1]
-            while mon_stack and mon_stack[-1]<=time:
-                mon_stack.pop()
-            mon_stack.append(time)    
+            mon_stack.append(time) 
+            if len(mon_stack)>=2 and mon_stack[-1]<=mon_stack[-2]:
+                mon_stack.pop()   
         
         return len(mon_stack)
