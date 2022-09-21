92d352db35de7ff8beb806b6f8509f500d3ea619
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-09-21 12:53:27 +0300

    Time: 69 ms (17.13%), Space: 14 MB (14.43%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -2,9 +2,8 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         insert=0
         for num in nums:
-            if num!=val:
-                nums[insert]=num
+            if num!= val:
+                nums[insert]= num
                 insert+=1
-        return insert        
-
+        return insert          
         
