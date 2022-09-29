f5da8e4a51edddce44618ab4c8346232db293e32
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-09-29 16:19:21 +0300

    Time: 356 ms (30.00%), Space: 15.5 MB (85.59%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,14 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        ans={}
-        max_=0
-        for n in nums:
-            if n not in ans.keys():
-                ans[n]=1
-            else:
-                ans[n]+=1  
-            res=n if max_<ans[n] else res    
-            max_=max(max_,ans[n])
-        
-        return res       
+        nums.sort()
+        return nums[len(nums)//2]
+         
        
