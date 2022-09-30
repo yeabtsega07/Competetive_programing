d8fcfaf53560abd8abf1d558d7da753acb654e45
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-09-30 12:24:40 +0300

    Time: 124 ms (82.05%), Space: 19.8 MB (27.83%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,20 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap={}
-        res=[]
-        for i,ana in enumerate(strs):
-            hashmap[i]=sorted(ana)
-        cur=[] 
-        
-        for  ana in sorted(hashmap.items(),key=lambda x:x[1]):
-            if cur and ana[1]!=sorted(cur[-1]):
-                res.append(cur)
-                cur=[]
-                cur.append(strs[ana[0]])
-            else:
-                cur.append(strs[ana[0]])
-        if cur: res.append(cur)        
-        return res       
+        dict=defaultdict(list)
+        for i in strs:
+            count=[0]*26
+            for s in i:
+                count[ord(s)-ord("a")]+=1
+            dict[tuple(count)].append(i) 
+        return dict.values()    
                 
             
         
