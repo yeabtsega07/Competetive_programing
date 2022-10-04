8744e74ac2821fe356d283e436909eccc0499f1b
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-04 22:07:20 +0300

    Time: 45 ms (94.52%), Space: 15.3 MB (49.16%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,12 +1,16 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        track={}
+        count={}
         ans=""
-        for char in s:
-            track[char]=track.get(char,0)+1
-        for val in sorted(track.items(), key=lambda x:x[1] , reverse=True):
-              ans+=val[0]*val[1]
+        for i in s:
+            if i not in count:
+                count[i]=1
+            else:
+                count[i]+=1
+        for i in sorted(count.items(),key=lambda x:x[1],reverse=True):
+              ans+=i[0]*i[1]
         return ans    
+   
         
         
             
