5450cc32a22f44d26349ceeb2a2d7fce6da4ad9d
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-05 13:35:24 +0300

    Time: 220 ms (63.91%), Space: 15.3 MB (5.52%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -5,18 +5,24 @@ class Solution:
         sdi={}
         pdi={}
         for i in range(len(p)):
+            
             sdi[s[i]]=sdi.get(s[i],0)+1
             pdi[p[i]]=pdi.get(p[i],0)+1
+            
         ans=[0] if sdi==pdi else []
         slow=0
         for fast in range(len(p),len(s)):
+            
             sdi[s[fast]]=sdi.get(s[fast],0)+1
             sdi[s[slow]]-=1
+            
             if sdi[s[slow]]==0:
                 sdi.pop(s[slow])
+                
             slow+=1
             if sdi==pdi:
                 ans.append(slow)
+                
         return ans        
                     
             
