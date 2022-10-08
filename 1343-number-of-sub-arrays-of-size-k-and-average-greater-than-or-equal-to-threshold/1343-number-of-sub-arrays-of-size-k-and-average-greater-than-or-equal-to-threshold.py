f0fa1cd5c68f73a1ca812083bfac1321cfddb75d
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-08 12:58:58 +0300

    Time: 1832 ms (9.95%), Space: 27.4 MB (24.78%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,18 +1,15 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        track=0
-        count=0
-        lft,rgt=0,0
+        sum_=0
+        slow,fast=0,0
         res=0
-        while rgt<len(arr):
-            track+=arr[rgt]
-            count+=1
-            if count==k:
-                if track/k>=threshold:
+        while fast<len(arr):
+            sum_+=arr[fast]
+            if (fast-slow)==k-1:
+                if sum_//k>=threshold:
                     res+=1
-                track-=arr[lft]
-                lft+=1
-                count-=1
-            rgt+=1
-        return res     
+                sum_-=arr[slow]
+                slow+=1
+            fast+=1
+        return res    
         
