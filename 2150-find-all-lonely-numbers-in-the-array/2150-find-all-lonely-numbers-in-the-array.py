0eb93b4433a7840bb2fd0a7b1948bc2d2beb0926
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-14 23:01:52 +0300

    Time: 3329 ms (29.71%), Space: 41.8 MB (21.39%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.py
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.py
@@ -2,16 +2,19 @@ class Solution:
     def findLonely(self, nums: List[int]) -> List[int]:
         if len(nums)==1:
             return nums
-        nums.sort()
-    
-        res=[]
+
+        cnt={}
         for i,n in enumerate(nums):
-            if i==0 and nums[i+1]!=n+1 and nums[i+1]!=n:
-                res.append(n)
-            elif i==len(nums)-1 and nums[i-1]!=n-1 and nums[i-1]!=n:
-                res.append(n)
-            elif 0<i<len(nums)-1 and nums[i+1]!=n+1 and nums[i+1]!=n and nums[i-1]!=n-1 and nums[i-1]!=n:
-                res.append(n)
+            if n not in cnt.keys():
+                cnt[n]=1
+            else:
+                cnt[n]+=1
+             
+        res=[]
+        for i in set(nums):
+            if i+1 not in cnt.keys() and i-1 not in cnt.keys() and cnt[i]==1:
+                res.append(i)
+ 
         
         return res
     
