71d8c651996bc36e04e3c668e10d5b27a20d971f
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-16 19:11:07 +0300

    Time: 541 ms (11.29%), Space: 13.9 MB (57.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,16 +3,20 @@ class Solution:
         track={}
         left=0
         ans=0
-        
+        sum_,max_=0,0
         for right in range(len(s)):
             
             track[s[right]] = 1+track.get(s[right],0)
+            sum_+=1
+            max_=max(track[s[right]],max_)
             
-            while sum(track.values())-max(track.values())>k:
+            while sum_-max_>k:
                 if track[s[left]]>1:
                     track[s[left]]-=1
                 else:
                     track.pop(s[left])
+                sum_-=1
+                max_=max(track.values())
                 left+=1    
             ans = max(ans,right-left+1)
          
