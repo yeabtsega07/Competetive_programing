9d0fc90973f6490ceaef7c46ab7798bf5e854a12
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-16 19:42:26 +0300

    Time: 343 ms (32.46%), Space: 14 MB (57.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,22 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        track={}
-        left=0
-        ans=0
-        sum_,max_=0,0
+        """
+        AABABBA
+        L   R  
+        4-0+1=5
+        5-3=2
+        {A:3,B:2}
+        MAX_=MAX(3,)
+        """
+        track=defaultdict(int)
+        left,ans,max_=0,0,0
+        
         for right in range(len(s)):
             
-            track[s[right]] = 1+track.get(s[right],0)
-            sum_+=1
+            track[s[right]] += 1
+
             max_=max(track[s[right]],max_)
             
-            while sum_-max_>k:
-                if track[s[left]]>1:
-                    track[s[left]]-=1
-                else:
-                    track.pop(s[left])
-                sum_-=1
-                left+=1    
+            while (right-left+1) -max_>k:
+                track[s[left]]-=1
+                left+=1 
+                
             ans = max(ans,right-left+1)
          
         return ans
