9833fd06393318fe40c77a1260407416375c59ef
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-17 18:53:36 +0300

    Time: 538 ms (28.47%), Space: 15.2 MB (91.72%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -1,11 +1,23 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        numSorted = sorted(nums)
+        stack = []
         start, end = float("inf"), 0
         
         for i,num in enumerate(nums):
-            if numSorted[i] != num:
-                start = min(start,i)
-                end = max(end,i)
-                
+            
+            while stack and nums[stack[-1]] > num:
+                start =  min(stack.pop(), start)
+            
+            stack.append(i)
+        
+        stack.clear()
+        
+        for i in range (len(nums)-1,-1,-1):
+            
+            while stack and nums[stack[-1]] < nums[i]:
+                end =  max(stack.pop(), end)
+            
+            stack.append(i)
+            
+       
         return end-start+1 if start< end else 0        
