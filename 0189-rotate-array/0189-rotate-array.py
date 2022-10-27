854bcb38fde7da3e0bf61d24a24bed28777f6e80
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-10-27 13:42:22 +0300

    Time: 571 ms (41.18%), Space: 25.3 MB (76.32%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,13 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         
         """
-        while k>len(nums):
-            k=k-len(nums)
-        slicer=len(nums)-k
-        for i in range(len(nums)):
-            if i==slicer:
-                break 
-            num=nums.pop(0)    
-            nums.append(num) 
-            
-        return nums
+        k = k % len(nums)
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed (nums[k:])  
+        
