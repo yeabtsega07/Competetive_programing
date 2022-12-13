7001566985f5d071af44f2d9824fc26c7045d4e0
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-12-13 09:38:19 +0300

    Time: 41 ms (75.91%), Space: 13.8 MB (96.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dict = {1:1,2:2}
-        def helper (n):
-            if n not in dict.keys():
-                dict[n] = helper(n-1) + helper(n-2)
-            return dict[n]
-        return helper(n)
-  
+        if n == 1:
+            return 1
+        dp = [0]*n
+        dp[0], dp[1] = 1, 2
         
+        for i in range(3, n + 1):
+            dp[i-1] = dp[i-2] + dp[i-3]
         
+        return dp[n-1]
