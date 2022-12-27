9ddcc5c1eee1fdb207b3756ce2a3a8eb565d86a3
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2022-12-27 09:39:34 +0300

    Time: 982 ms (83.23%), Space: 22 MB (87.83%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -3,19 +3,11 @@ class Solution:
         n = len(rocks)
         track, count = [], 0
         for i in range(n):
-            if rocks[i] < capacity[i]:
-                diff = capacity[i] - rocks[i]
-                track.append(diff)
-            else:
-                count += 1
-
-        heapify(track)
-        i, n = 0, len(track)
-        while i < n and additionalRocks:
-            diff = heappop(track)
-            additionalRocks -= diff
-            i += 1
+            diff = capacity[i] - rocks[i]
+            heappush(track, diff)
+  
+        for i in range(n):
+            additionalRocks -= heappop(track)
             if additionalRocks >= 0:
                 count += 1  
         return count        
-        
