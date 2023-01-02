e7f9b1513a3a4fbe20a6df1238c559763bbd922d
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2023-01-02 10:14:38 +0300

    Time: 39 ms (63.58%), Space: 13.8 MB (99.08%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,18 +1,33 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        track = {}
-        s = s.split()
-        if len(pattern) != len(s):
+    def wordPattern(self, pattern: str, string: str) -> bool:
+
+        # Create a dictionary to map characters in the pattern to words
+        chartoword = {}
+        # Create a set to store the words we have seen so far
+        seenwords = set()
+        # Split the string into a list of words
+        words = string.split()
+
+        # If the pattern and the list of words have different lengths,
+        # then they cannot match
+        if len(pattern) != len(words):
             return False
-        
+
+        # Iterate through the characters in the pattern
         for i, char in enumerate(pattern):
-            if char in track:
-                if track[char] != s[i]:
+            # If the character is not in the mapping yet,
+            # add it and the corresponding word to the mapping
+            if char not in chartoword:
+                chartoword[char] = words[i]
+                # If the word has already been added to the mapping,
+                # then this pattern is invalid
+                if words[i] in seenwords:
                     return False
-            else:
-                track[char] = s[i]
-                
-        if len(track.keys()) != len(set(track.values())): 
-            return False
+                seenwords.add(words[i])
+            # If the character is in the mapping,
+            # but it maps to a different word, then this pattern is invalid
+            elif chartoword[char] != words[i]:
+                return False
+
+        # If we have not returned False yet, then the pattern is valid
         return True
-        
