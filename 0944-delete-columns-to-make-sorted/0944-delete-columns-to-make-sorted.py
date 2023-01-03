be34763b796895fd0dd10ec6ebdfda4326ab4fd3
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2023-01-03 09:32:32 +0300

    Time: 497 ms (30.53%), Space: 14.7 MB (61.89%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,15 +1,12 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        track = defaultdict(list)
-        
-        for i in range(len(strs)):
-            for j in range(len(strs[i])):
-                track[j].append(strs[i][j])
-        
         ans = 0
-        for i in track.keys():
-            if track[i] != sorted(track[i]):
-                ans += 1
+        
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if ord(strs[j][i]) < ord(strs[j - 1][i]):
+                    ans += 1
+                    break    
         
         return ans
         
