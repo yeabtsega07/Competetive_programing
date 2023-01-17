3221d3b71090a21d096d97ee47b3b92850592e0c
yeabtsega07 <106152360+yeabtsega07@users.noreply.github.com>
2023-01-17 20:09:38 +0300

    Time: 360 ms (16.03%), Space: 14.3 MB (99.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,18 +1,21 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
 
-        for i in range(1, len(heights)):
-            
-            cur_name = names[i]
-            cur_height = heights[i]
-            ind = i 
-            while ind > 0 and cur_height > heights[ind - 1]:
-                heights[ind] = heights[ind - 1]
-                names[ind] = names[ind - 1]
-                ind -= 1
-            
-            names[ind] =  cur_name
-            heights[ind] = cur_height
-                       
-        return names
+        track = defaultdict(list)
+        max_ = 0
+        
+        for i, name in enumerate(names):
+            track[heights[i]].append(name)
+            max_ = max(max_, heights[i])
+        
+        ans = []
+        # print(track, max_)
+        for i in reversed(range(max_ + 1)):
+            if i in track:
+                for x in track[i]:
+                    ans.append(x)
+        
+        return ans
+        
+        
         
