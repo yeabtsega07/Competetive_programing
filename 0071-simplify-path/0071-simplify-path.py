b76f7dab170430ea194889bbbb8257d1fa52b31e
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-02-28 16:01:04 +0300

    Time: 30 ms (89.04%), Space: 13.8 MB (98.34%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,40 +1,20 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack, cur = [] , []
         
-        for i in range(len(path)):
-            # print(cur, i, stack) 
-            if path[i] == "/" and cur:
-                if cur == ["/",".","."]:
-
-                    if stack:
-                        stack.pop()
-                        cur = ["/"]
-                    cur = ["/"]   
-                elif cur == ["/","."]:
-                    cur = ["/"]
-                    
-                else:
-                    if len(cur) > 2 and cur[0] == cur[1] == "/":
-                        stack.append("".join(cur[1:]))
-                    elif cur == ["/"] :
-                        cur = ["/"]
-                    else:
-                        stack.append("".join(cur))
-                        
-                    cur = ["/"]
-                    
-            else:
-                
-                cur.append(path[i])
+        stack, path = [], path.split("/")
         
-        if cur == ["/",".","."]:
-            if stack:
-                stack.pop()
-                cur = ["/"]
-        elif cur and len(cur) > 1 and cur != ["/","."]:
-            stack.append("".join(cur))
+        for dir in path:
             
-        return "".join(stack) if stack else "/"    
+            if dir == "..":
+                
+                if  stack:
+                    stack.pop()
+            
+            elif dir and dir != ".":
+                stack.append(dir)
+        
+        return "/" + "/".join(stack)
+            
+            
                 
         
