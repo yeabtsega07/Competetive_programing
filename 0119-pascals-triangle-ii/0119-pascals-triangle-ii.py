b69b60a088272b59c92be2aa0d8500009f7befa1
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-03-01 11:00:45 +0300

    Time: 19 ms (99.66%), Space: 13.9 MB (10.53%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         
-        @cache
+        # @cache
         def recur(rowIndex):
             
             if rowIndex == 0:
                 return [1]
             
-            arr = []
+            arr, cur = [], recur(rowIndex - 1) 
             for i in range( rowIndex - 1):
-                arr.append( recur( rowIndex - 1)[i] + recur(rowIndex - 1)[i + 1])
+                
+                arr.append( cur[i] + cur[i + 1])
             
             arr = [1] + arr + [1]
             return arr
