20f619728faf91dfd8a821b98a0b6e4990179467
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-03-03 00:23:00 +0300

    Time: 56 ms (84.40%), Space: 14.1 MB (23.40%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    
+        res , track = [], []
+        
+        def recur( cand, target, res, index, track):
+            
+            if index == len(cand):
+                if target == 0:
+                    res.append(track[:])
+                return
+            
+            if cand[index] <= target:
+                track.append(cand[index])
+                recur( cand, target - cand[index], res, index, track)
+                track.pop()
+            
+            recur( cand , target, res, index + 1, track)
+        
+        recur(candidates, target, res, 0, track)
+        return res
