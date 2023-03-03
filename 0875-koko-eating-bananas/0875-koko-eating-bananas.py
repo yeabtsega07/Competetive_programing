728658057e405f474ccb72b1c14f02087e0c5e07
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-03-03 14:27:00 +0300

    Time: 382 ms (96.10%), Space: 15.5 MB (16.86%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,26 +3,26 @@ class Solution:
         def check(nums, k):
             count = 0
             for num in nums:
-                
+
                 count += ceil(num / k)
             
             return count
         
         left, right = 1, max(piles)
-        ans = 0
+
         while left <= right:
             
             mid = left + (right - left) // 2
             
             if check(piles, mid) <= h:
-                ans = mid
+
                 right = mid - 1
             
             else:
                 left = mid + 1
 
-        
-        return ans
+
+        return right if right and check(piles, right) <= h else right + 1
             
 
             
