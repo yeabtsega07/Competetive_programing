0e8f94851bf137fad5b83bd92f80a617b4f7d72e
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-03-08 08:04:08 -0800

    Time: 144 ms (71.40%), Space: 20.6 MB (70.56%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,39 +1,19 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-#         pos = []
         
-#         def bisect( nums ):
-            
-#             left, right = 0, max(citations)
-            
-#             while left <= right:
-                
-#                 mid = left + (right - left) // 2
-                
-#                 check = bisect_left(citations, mid)
-#                 if mid <= len(nums) - check :
-
-#                     pos.append([mid, len(nums) - check])
-#                     left = mid + 1
-#                 else:
-#                     right = mid - 1
+        low, high = 0, len(citations) - 1
         
-#         bisect(citations)
-#         # print(pos)
-#         return pos[-1][0]
-        left , right = 0 , len(citations) - 1
+        while low <= high:
+            
+            mid = low + (high  - low) // 2
 
-        while left <= right:
-            
-            mid = left + (right - left) // 2
-            
-            if citations[mid] == len(citations) - mid:
+            if citations[mid] == len(citations) - mid :
                 return citations[mid]
-            
-            elif citations[mid] > len(citations) - mid:
-                right = mid - 1
+            elif citations[mid] > len(citations) - mid :
+                high = mid - 1
             else:
-                left = mid + 1
-                
-        return len(citations) - left
+                low = mid + 1
+        
+        return len(citations) - low
+
         
