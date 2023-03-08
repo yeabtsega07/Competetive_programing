bbb995724177dcd37f24ed551df7819bbf10544e
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-03-08 08:27:55 -0800

    Time: 276 ms (96.87%), Space: 20.2 MB (35.42%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -1,37 +1,19 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        starts = { intervals[i][0]:i  for i in range(len(intervals))}
-        listOfStarts = sorted(starts.keys())
         
-        def custom_bisect_left ( end ):
-            
-            left = -1
-            right = len(listOfStarts)
-            
-            while right > left + 1 :
-                
-                mid = left + (right - left) // 2
-                
-                if listOfStarts[ mid ] < end:
-                    left = mid
-                    
-                else:
-                    right = mid
-            
-            return right if right < len(listOfStarts) else -1
-        
-        
-        result = []
-        for start, end in intervals:
-            
-            check  = custom_bisect_left( end )
-            if check == -1:
-                result.append(-1)
-            else:    
-                result.append(starts[listOfStarts[check]])
+        track = { intervals[i][0]:i for i in range(len(intervals))}
+        starts = sorted(track.keys())
 
-                
+        result = []
+        
+        for start, end  in intervals:
+            
+            temp = bisect_left(starts, end )
+            if temp >= len(starts):
+                result.append(-1)
+            else:
+                result.append(track[starts[temp]])
         
         return result
-        
-        
+                
+                
