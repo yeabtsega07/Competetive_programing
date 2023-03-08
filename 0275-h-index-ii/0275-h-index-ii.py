f2c7f2360bc6f8e84e10effe13efd49a793131b2
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-03-08 00:42:01 -0800

    Time: 141 ms (80.20%), Space: 20.5 MB (70.56%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,24 +1,39 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        pos = []
+#         pos = []
         
-        def bisect( nums ):
+#         def bisect( nums ):
             
-            left, right = 0, max(citations)
+#             left, right = 0, max(citations)
             
-            while left <= right:
+#             while left <= right:
                 
-                mid = left + (right - left) // 2
+#                 mid = left + (right - left) // 2
                 
-                check = bisect_left(citations, mid)
-                if mid <= len(nums) - check :
+#                 check = bisect_left(citations, mid)
+#                 if mid <= len(nums) - check :
 
-                    pos.append([mid, len(nums) - check])
-                    left = mid + 1
-                else:
-                    right = mid - 1
+#                     pos.append([mid, len(nums) - check])
+#                     left = mid + 1
+#                 else:
+#                     right = mid - 1
         
-        bisect(citations)
-        # print(pos)
-        return pos[-1][0]
+#         bisect(citations)
+#         # print(pos)
+#         return pos[-1][0]
+        left , right = 0 , len(citations) - 1
+
+        while left <= right:
+            
+            mid = left + (right - left) // 2
+            
+            if citations[mid] == len(citations) - mid:
+                return citations[mid]
+            
+            elif citations[mid] > len(citations) - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return len(citations) - left
         
