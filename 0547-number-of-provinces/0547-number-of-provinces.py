7fc58775d267055ac7cee881dea216558083a054
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-05-17 16:54:28 +0300

    Time: 221 ms (15.18%), Space: 16.7 MB (29.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,32 +1,55 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
-        graph = defaultdict(list)
-        
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[0])):
-                if isConnected[i][j]:
-                    graph[i].append(j)
-                    graph[j].append(i)
-        
-        visited = set()
-        
-        def dfs ( node, visited ):
-            
-            visited.add(node)
-            
-            for child in graph[node]:
+        class UnionFind:
+            def __init__(self, size):
+                self.root = { i:i for i in range ( size ) }
+                self.rank = [0] * size
+                self.size = size
+
+            def find(self, member):
+
+                if member == self.root[member]:
+                    return member
+                self.root[member] = self.find(self.root[member])
+                return self.root[member]
+
+            def union(self, x, y):
                 
-                if child not in visited:
-                    dfs(child, visited)
-        
-        result = 0
-        
-        for key in graph.keys():
+                rootX = self.find(x)
+                rootY = self.find(y)
+                
+                if rootX != rootY:
+                    
+                    if self.rank[rootX] > self.rank[rootY]:
+                        
+                        self.root[rootY] = rootX
+                        
+                    elif self.rank[rootX] < self.rank[rootY]:
+                        
+                        self.root[rootX] = rootY
+                        
+                    else:
+                        
+                        self.root[rootY] = rootX
+                        self.rank[rootX] += 1
+
+
+            def connected(self, x, y):
+                return self.root[x] == self.root[y]
             
-            if key not in visited:
-                dfs(key, visited)
-                result += 1
+        check = UnionFind(len(isConnected))
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
+                
+                if isConnected[i][j]:
+                    check.union(i,j)
         
-        return result
+        parent = 0
+        for i in range(len(isConnected)):
+            if check.root[i] == i:
+                parent += 1
+        
+        return parent
+
                 
