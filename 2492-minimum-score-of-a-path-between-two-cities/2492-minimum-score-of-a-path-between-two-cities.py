a9bfbc9a919d1b1189f80a332d14a21cf2622600
Yeabtsega Yifat <106152360+yeabtsega07@users.noreply.github.com>
2023-05-18 19:16:37 +0300

    Time: 1898 ms (30.21%), Space: 61.1 MB (81.40%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,29 +1,58 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         
-        graph = defaultdict(list)
+        class UnionFind:
+            def __init__(self, size):
+                self.root = { i:i for i in range ( size ) }
+                self.rank = [0] * size
+                self.size = size
+
+            def find(self, member):
+
+                if member != self.root[member]:
+                    self.root[member] = self.find(self.root[member])
+                
+                return self.root[member]
+
+            def union(self, x, y):
+                
+                rootX = self.find(x)
+                rootY = self.find(y)
+                
+                if rootX != rootY:
+                    
+                    if self.rank[rootX] > self.rank[rootY]:
+                        
+                        self.root[rootY] = rootX
+                        
+                    elif self.rank[rootX] < self.rank[rootY]:
+                        
+                        self.root[rootX] = rootY
+                        
+                    else:
+                        
+                        self.root[rootY] = rootX
+                        self.rank[rootX] += 1
+
+
+            def connected(self, x, y):
+                return self.find(x) == self.find(y)
+            
+        check = UnionFind(n)
 
         for i, j, dis in roads:
             
-            graph[i].append((j,dis))
-            graph[j].append((i,dis))
+            check.union(i - 1,j - 1)
 
-        queue = deque([1])
-        visited, result = set(), float("inf")
-        while queue:
+        
+        
+        result = float("inf")
+        for i, j, dis in roads:
             
-            node = queue.popleft()
-
-            for child in graph[node]:
-                
-                result = min(result, child[1])
-                
-                if child[0] not in visited:
-                    visited.add(child[0])
-                    queue.append(child[0])
+            if check.connected(i - 1,0) or check.connected(i - 1, n - 1):
+                result = min(result, dis)
+            
+               
+            
         
         return result
-                
-                
-                
-                
