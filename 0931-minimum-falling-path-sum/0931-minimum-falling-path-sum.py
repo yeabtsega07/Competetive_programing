ca3469ce3f690792c5b912c4bd3a25880f16da2c
Yeabtsega Yifat <yeabtse7@gmail.com>
2023-07-26 00:35:39 +0300

    Time: 229 ms (5.16%), Space: 26.2 MB (7.20%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,20 +1,43 @@
 class Solution:
 
     def minFallingPathSum(self, matrix: list[list[int]]) -> int:
+        
+        def inBound( row, col ):
+            return 0 <= row < len(matrix) and 0 <= col < len(matrix[0])
+        
+        dp = {}
+        
+        def recur ( row, col ):
+            
+            if (row, col) in dp:
+                return dp[(row, col)]
+            
+            if row == 0 and inBound(row, col):
+                return matrix[row][col]
+            
+            if not inBound(row, col):
+                return float("inf")
+            
+            topLeft = recur(row - 1, col - 1)
+            top = recur(row - 1, col)
+            topRight = recur(row - 1, col + 1)
 
-        m = len(matrix)
+            dp[(row, col)] = min(topLeft, top, topRight) + matrix[row][col]
+            
+            return dp[(row, col)]
+        
+        row, result = len(matrix) - 1, float("inf")
+        
+        for col in range(len(matrix[0])):
+            result = min( recur(row, col), result)
+        
+        return result
+            
+            
+            
+            
+            
+            
 
-        matrix = [[inf] + x + [inf] for x in matrix]              
-
-                                                            
-
-        for i in range(m - 2,-1,-1):
-
-            for j in (range(1,m + 1)):
-
-                matrix[i][j] += min(matrix[i + 1][j - 1 : j + 2])   
-
-                
-
-        return min(matrix[0])
+        
         
