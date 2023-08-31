c8f6dee3c48dd510ac4f37da5401140093b3cb57
Yeabtsega Yifat <yeabtse7@gmail.com>
2023-08-31 14:54:13 +0300

    Time: 93 ms (66.37%), Space: 16.5 MB (93.83%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,20 +1,37 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums = []
-        left, right = 0 , 0
         
-        while left < len(nums1) and right < len(nums2):
+        n1, n2 = nums1, nums2
+        
+        if len(nums2) < len(nums1) :
+            n1 = nums2
+            n2 = nums1
+        
+        total = len(n1) + len(n2)
+        half = total // 2
+        
+        left, right = 0, len(n1) - 1
+        
+        while True:
             
-            if nums1[left] <= nums2[right]:
-                nums.append(nums1[left])
-                left += 1
+            mid1 = (left + right) // 2
+            mid2 = half - (mid1 + 2)
+            
+            n1Mid = n1[mid1] if mid1 >= 0 else -float("inf")
+            n1Right = n1[mid1 + 1] if (mid1 + 1) < len(n1) else float("inf")
+            n2Mid = n2[mid2] if mid2 >= 0 else -float("inf")
+            n2Right = n2[mid2 + 1] if (mid2 + 1) < len(n2) else float("inf")
+            
+            if n1Mid <= n2Right and n2Mid <= n1Right:
+                
+                if total % 2:
+                    return min(n1Right, n2Right)
+                return (max(n1Mid, n2Mid ) + min(n1Right, n2Right)) / 2
+            
+            if n1Mid > n2Right:
+                right = mid1 - 1
             else:
-                nums.append(nums2[right])
-                right += 1
+                left = mid1 + 1
+                
         
-        nums.extend(nums1[left :])
-        nums.extend(nums2[right :])
-        if len(nums) % 2:
-            return nums[len(nums) // 2]
-        else:
-            return (nums[len(nums) // 2] + nums[len(nums) // 2 - 1]) / 2 
+        
