cc675980087e7c3aee77158f7d5ce3692ae539c7
Yeabtsega Yifat <yeabtse7@gmail.com>
2023-10-31 23:55:00 +0300

    Time: 153 ms (26.94%), Space: 17.4 MB (96.33%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
@@ -1,7 +1,7 @@
 class Solution:
     def findBestValue(self, arr: List[int], target: int) -> int:
         
-        cur = int(target / len(arr) + 0.5)
+        cur = round(target / len(arr))
         
         less = 0
         for num in arr:
@@ -18,11 +18,9 @@ class Solution:
             return res
         
         low , high = cur, max(arr)
-        # print(low, high, "start", less)
         while low < high:
             
             mid = low + (high - low) // 2
-            # print(mid, calc(mid), low, high)
             if calc(mid) == target:
                 return mid
             elif calc(mid) < target:
@@ -30,7 +28,7 @@ class Solution:
             else:
                 high = mid 
         
-        # print(low, high, "end")
+
         if abs(calc(high - 1) - target) <= abs(calc(high) - target):
             return high - 1
         return high
