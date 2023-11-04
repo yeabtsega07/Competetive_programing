a2ab5e4ab15ff3df5b4620bfb964bfd66bbc66c5
Yeabtsega Yifat <yeabtse7@gmail.com>
2023-11-04 22:09:54 +0300

    Time: 67 ms (9.46%), Space: 17.8 MB (27.11%) - LeetHub

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+        
+        @lru_cache
+        def recur ( index, current_height, remaining_w):
+            
+            # base case
+            if index >= len(books):
+                return current_height
+            
+            # First choice place the book on the current shelf
+            ans = float("inf")
+            if books[index][0] <= remaining_w:
+                ans = min(ans, recur(index + 1, max(current_height, books[index][1]), remaining_w - books[index][0]))
+            
+            # Second choice place the book on the next shelf
+            
+            ans = min(ans, current_height + recur(index + 1, books[index][1], shelfWidth - books[index][0]))
+            
+            return ans
+        
+        return recur(0, 0, shelfWidth)
