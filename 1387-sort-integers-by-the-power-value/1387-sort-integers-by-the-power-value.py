5a0a55046960ca27592b4e329f89679b63600a62
Yeabtsega Yifat <yeabtse7@gmail.com>
2023-11-21 21:27:16 +0300

    Time: 103 ms (97.32%), Space: 31.8 MB (12.19%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,27 +1,27 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         
-        memo = {1:0}
+        track = defaultdict(int)
+        memo = {1 : 1}
         
         def get_power(num):
-            
+
             if num in memo:
                 return memo[num]
             
             if num % 2:
-                cur = 1 + get_power(num * 3 + 1)
+                ans = 1 + get_power(3 * num + 1)
             else:
-                cur = 1 + get_power(num // 2)
+                ans = 1 + get_power(num // 2)
             
-            return cur
-        
-        nums = {}
+            memo[num] = ans
+            
+            return ans
         
         for num in range(lo, hi + 1):
             
-            nums[num] = get_power(num)
+            track[num] = get_power(num)
+            
         
-        nums = sorted(nums.items(), key = lambda x:x[1])
-
-        return nums[k - 1][0]
+        return sorted(track.items(), key = lambda x:x[1])[k - 1][0]
             
