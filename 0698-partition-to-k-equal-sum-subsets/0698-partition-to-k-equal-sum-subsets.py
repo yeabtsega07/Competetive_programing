fc72e8135e6760b69ee97a15fdb48263e3979d91
Yeabtsega Yifat <yeabtse7@gmail.com>
2024-05-16 18:38:01 +0300

    Time: 766 ms (16.37%), Space: 121.4 MB (5.19%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,22 +1,25 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         summ = sum(nums)
-        nums.sort(reverse=True)
         if summ % k != 0:
             return False
         value = summ//k
-
         @cache
-        def backTrack(index,arr):
-            if index == len(nums):
-                return min(arr) == value
-            answer = False
-            for i in range(len(arr)):
-                if arr[i] + nums[index] <= value:
-                    new_arr = list(arr)
-                    new_arr[i] += nums[index]
-                    answer = answer or backTrack(index+1,tuple(new_arr))
-                    # arr[i] -= nums[index]
-            return answer
+        def dp(bitmask,k,target):
+            if target < 0:
+                return False
+            if target == 0:
+                k -= 1
+                target = value
+            if k == 0:
+                return bitmask+1 == 2**len(nums)
+            
+            ans = False
+            for i in range(len(nums)):
+                if bitmask & (1 << i) == 0:
+                    bitmask ^= (1<<i)
+                    ans = ans or dp(bitmask,k,target-nums[i])
+                    bitmask ^= (1<<i)
+            return ans
 
-        return backTrack(0,tuple([0 for _ in range(k)]))
+        return dp(0,k,value)
