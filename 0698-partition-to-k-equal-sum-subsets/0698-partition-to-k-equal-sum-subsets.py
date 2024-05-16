1871a8e7e7df8a6e8b5e04192c7ab451e9e22843
Yeabtsega Yifat <yeabtse7@gmail.com>
2024-05-16 18:45:20 +0300

    Time: 778 ms (15.83%), Space: 121.3 MB (5.19%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -5,21 +5,22 @@ class Solution:
             return False
         value = summ//k
         @cache
-        def dp(bitmask,k,target):
+        def dp(bitmask, target):
             if target < 0:
                 return False
-            if target == 0:
-                k -= 1
-                target = value
-            if k == 0:
-                return bitmask+1 == 2**len(nums)
             
+            if bitmask+1 == 2**len(nums):
+                return target == 0
+            
+            if target == 0:
+                target = value
+                
             ans = False
             for i in range(len(nums)):
                 if bitmask & (1 << i) == 0:
                     bitmask ^= (1<<i)
-                    ans = ans or dp(bitmask,k,target-nums[i])
+                    ans = ans or dp(bitmask,target-nums[i])
                     bitmask ^= (1<<i)
             return ans
 
-        return dp(0,k,value)
+        return dp(0,value)
